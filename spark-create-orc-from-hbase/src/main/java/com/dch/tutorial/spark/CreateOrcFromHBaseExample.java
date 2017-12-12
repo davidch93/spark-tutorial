277663a31eed57b8ef0f5ac6ea15ee1443c6e9be
bukalapak <david.christianto1@gmail.com>
2017-12-12 16:50:17 +0700

    Initialize commit spark-create-orc-from-hbase project.

---
+++ spark-create-orc-from-hbase/src/main/java/com/dch/tutorial/spark/CreateOrcFromHBaseExample.java
@@ -0,0 +1,16 @@
+package com.dch.tutorial.spark;
+
+/**
+ * Example create ORC from HBase file using Spark.
+ * 
+ * @author David.Christianto
+ */
+public class CreateOrcFromHBaseExample {
+
+	public static final String HDFS_PATH = "hdfs://localhost:54310";
+	public static final String ORC_WAREHOUSE_PATH = HDFS_PATH + "/user/hive/warehouse/";
+
+	public static void main(String... args) {
+		
+	}
+}
